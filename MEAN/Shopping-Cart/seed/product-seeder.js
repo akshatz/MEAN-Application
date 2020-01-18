14e12314c392aabbdd3aed65e908e59e15af6d44
Akshat Zala <akshatzala@gmail.com>
2020-01-18 16:42:02 +0530

    Seeding Data

---
+++ MEAN/Shopping-Cart/seed/product-seeder.js
@@ -0,0 +1,8 @@
+var Product = require('../models/product');
+
+var product = new Product({
+    imagePath: 'https://upload.wikimedia.org/wikipedia/en/5/5e/Gothiccover.png', 
+    title: 'GOTHIC Video Game',
+    description: 'Awesome Game!!!',
+    price: 10
+});
