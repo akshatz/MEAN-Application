f1c260e674af940f06ae922446dd861278fd4116
Akshat Zala <akshatzala@gmail.com>
2020-01-18 17:32:08 +0530

    Added gitignore

--- MEAN/Shopping-Cart/seed/product-seeder.js
+++ MEAN/Shopping-Cart/seed/product-seeder.js
@@ -1,8 +1,25 @@
 var Product = require('../models/product');
 
-var product = new Product({
+var mongoose = require('mongoose');
+
+mongoose.connect('localhost:27017/shopping');
+
+var products = new Product({
     imagePath: 'https://upload.wikimedia.org/wikipedia/en/5/5e/Gothiccover.png', 
     title: 'GOTHIC Video Game',
     description: 'Awesome Game!!!',
     price: 10
 });
+var done;
+for (var i = 0; i < products.length; i++){
+    products[i].save(function(err, result){
+        done ++;
+        if (done === products.length){
+            exit()
+        }
+    });
+}
+
+function exit(){
+    mongoose.disconnect();
+}
